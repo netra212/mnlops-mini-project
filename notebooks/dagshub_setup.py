4d709142665c840f250cfe0827e76fa6c0b8372a
netr212 <netra200021kcbdr@gmail.com>
2024-12-27 13:55:08 +0000

    add dvc pipeline code

--- notebooks/dagshub_setup.py
+++ notebooks/dagshub_setup.py
@@ -1,9 +1,8 @@
 import dagshub
 import mlflow
 
-mlflow.set_tracking_uri("https://dagshub.com/netra200021kcbdr/mnlops-mini-project.mlflow")
-
-dagshub.init(repo_owner='netra200021kcbdr', repo_name='mnlops-mini-project', mlflow=True)
+mlflow.set_tracking_uri("https://dagshub.com/netra200021kcbdr/mlops-mini-project.mlflow")
+dagshub.init(repo_owner='netra200021kcbdr', repo_name='mlops-mini-project', mlflow=True)
 
 with mlflow.start_run():
   mlflow.log_param('parameter name', 'value')
