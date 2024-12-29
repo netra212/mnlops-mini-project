f9e05b67aeaf6301b1df465ca710587d3ceee5dc
netr212 <netra200021kcbdr@gmail.com>
2024-12-29 14:21:52 +0000

    some minor bugs fixed

--- src/model/register_model.py
+++ src/model/register_model.py
@@ -6,7 +6,7 @@ import os
 import dagshub
 
 # Set up DagsHub credentials for MLflow tracking
-dagshub_token = os.getenv("DAGSHUB_PAT")
+dagshub_token = "2d69801e8579a94a3b15b40819f6d05f8498b943"
 if not dagshub_token:
     raise EnvironmentError("DAGSHUB_PAT environment variable is not set")
 
